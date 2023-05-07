cc7d46ec920a56c5d1bec50b33dcaf28bcfd9a58
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-05-07 12:49:11 -0500

    progress

--- lua/autorun.lua
+++ lua/autorun.lua
@@ -1,34 +1,32 @@
 local M = {}
 
--- I configured the key mapping <leader>x
--- to write and source the current buffer.
--- This is useful when developing a Neovim plugin.
---
-
 local function attach_to_buffer(bufnr, pattern, command)
-  -- read ":help autocmd"
+  function run()
+    local function append_data(_, data)
+      -- This inserts or replaces lines of text at a given buffer line.
+      -- 1st argument is the buffer number.
+      -- 2nd argument is the starting line number (-1 for end).
+      -- 3rd argument is the ending line number.
+      -- 4th argument is whether an error can be raised.
+      -- 5th argument is a list of lines to be written.
+      -- TODO: How can you make this text red?
+      vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
+    end
+
+    vim.fn.jobstart(command, {
+      stdout_buffered = true, -- only send complete lines
+      on_stdout = append_data,
+      on_stderr = append_data
+    })
+  end
+
   vim.api.nvim_create_autocmd("BufWritePost", {
     group = vim.api.nvim_create_augroup("RMV", { clear = true }),
     pattern = pattern,
-    callback = function()
-      local function append_data(_, data)
-        -- This inserts or replaces lines of text at a given buffer line.
-        -- 1st argument is the buffer number.
-        -- 2nd argument is the starting line number (-1 for end).
-        -- 3rd argument is the ending line number.
-        -- 4th argument is whether an error can be raised.
-        -- 5th argument is a list of lines to be written.
-        -- TODO: How can you make this text red?
-        vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
-      end
-
-      vim.fn.jobstart(command, {
-        stdout_buffered = true, -- only send complete lines
-        on_stdout = append_data,
-        on_stderr = append_data
-      })
-    end
+    callback = run
   })
+
+  run()
 end
 
 M.setup = function()
