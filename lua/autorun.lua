b3d01d8a51406824ac456dd568da39562c1429c4
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-05-07 19:56:16 -0500

    progress

--- lua/autorun.lua
+++ lua/autorun.lua
@@ -1,22 +1,39 @@
+-- CHANGE THIS
+
+--[[ TODO: Get this to work!
+local bufnr = vim.api.nvim_get_current_buf()
+local hl_group = vim.api.nvim_get_hl_by_name("Error", true)
+print("hl_group =", hl_group)
+-- vim.api.nvim_buf_add_highlight(bufnr, 0, "Error", 1, 1, 80)
+vim.api.nvim_buf_set_extmark(bufnr, 0, hl_group, 1, {})
+vim.api.nvim_buf_set_lines(bufnr, 1, 1, false, { "-- Hello, World!", "-- Line #2" }) ]]
 local M = {}
 
 local function attach_to_buffer(bufnr, pattern, command)
   function run()
-    local function append_data(_, data)
+    local function append_data(isError, data)
       -- This inserts or replaces lines of text at a given buffer line.
       -- 1st argument is the buffer number.
       -- 2nd argument is the starting line number (-1 for end).
       -- 3rd argument is the ending line number.
       -- 4th argument is whether an error can be raised.
       -- 5th argument is a list of lines to be written.
+
       -- TODO: How can you make this text red?
+      -- TODO: Use vim.api.nvim_buf_set_extmark instead?
+      -- TODO: Use the "Error" highlight group.
+      --[[ if error then
+        local hl_group = vim.api.nvim_get_hl_by_name("Error")
+        vim.api.nvim_buf_add_highlight(bufnr, 0, hl_group, 1, 1, 80)
+        -- vim.api.nvim_buf_set_extmark(bufnr, 0, hl_group, 1)
+      end ]]
       vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
     end
 
     vim.fn.jobstart(command, {
       stdout_buffered = true, -- only send complete lines
-      on_stdout = append_data,
-      on_stderr = append_data
+      on_stdout = function(_, data) append_data(false, data) end,
+      on_stderr = function(_, data) append_data(true, data) end
     })
   end
 
@@ -33,11 +50,18 @@ M.setup = function()
   vim.api.nvim_create_user_command(
     "AutoRun",
     function()
-      local bufnum = vim.fn.input "Buffer Number: "
+      -- TODO: Do the equivalent of :vnew.
+      vim.api.nvim_command("vnew")
+      -- TODO: Get the buffer number.
+      local bufnr = vim.api.nvim_get_current_buf()
+      -- TODO: Move focus back to the previous buffer.
+      vim.api.nvim_command("bprevious")
+      -- TODO: Now you won't need to prompt for the buffer number below.
+      -- local bufnr = vim.fn.input "Buffer Number: "
       local pattern = vim.fn.input "File Pattern: "
       local command = vim.fn.input "Command: "
       local words = vim.split(command, " ")
-      attach_to_buffer(tonumber(bufnum), pattern, words)
+      attach_to_buffer(tonumber(bufnr), pattern, words)
     end,
     {}
   )
