33a257122beeed4a5a6812302cba1309ba4fa258
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-05-09 12:57:53 -0500

    not giving up

--- lua/autorun.lua
+++ lua/autorun.lua
@@ -1,6 +1,10 @@
 local function attach_to_buffer(bufnr, pattern, command)
+  print("attached_to_buffer called")
+
   function run()
-    local function highlight_line(bufnr, line, hl_group)
+
+    local function highlight_line(line, hl_group)
+      print("highlight_line: hl_group =", hl_group)
       local ns_id = -1 -- ungrouped
       local col_start = 0 -- beginning of line
       local col_end = -1 -- end of line
@@ -8,36 +12,24 @@ local function attach_to_buffer(bufnr, pattern, command)
     end
 
     local function append_data(isError, data)
-      -- This inserts or replaces lines of text at a given buffer line.
-      -- 1st argument is the buffer number.
-      -- 2nd argument is the starting line number (-1 for end).
-      -- 3rd argument is the ending line number.
-      -- 4th argument is whether an error can be raised.
-      -- 5th argument is a list of lines to be written.
 
-      -- TODO: How can you make this text red?
-      -- TODO: Use vim.api.nvim_buf_set_extmark instead?
-      -- TODO: Use the "Error" highlight group.
-      --[[ if error then
-        local hl_group = vim.api.nvim_get_hl_by_name("Error")
-        vim.api.nvim_buf_add_highlight(bufnr, 0, hl_group, 1, 1, 80)
-        -- vim.api.nvim_buf_set_extmark(bufnr, 0, hl_group, 1)
-      end ]]
       -- Replace all existing lines
       if #data then
         local start = isError and -1 or 0
         local title = isError and "stderr" or "stdout"
 
-        --[[ local ns_id = vim.api.nvim_create_namespace('autorun')
-        vim.api.nvim_buf_set_extmark(bufnr, ns_id, start, 0, {
-          hl_group = "Error",
-          hl_eol = true
-        }) ]]
+        -- This inserts or replaces lines of text at a given buffer line.
+        -- 1st argument is the buffer number.
+        -- 2nd argument is the starting line number (-1 for end).
+        -- 3rd argument is the ending line number.
+        -- 4th argument is whether an error can be raised.
+        -- 5th argument is a list of lines to be written.
         vim.api.nvim_buf_set_lines(bufnr, start, -1, false, { title })
-        highlight_line(bufnr, -1, "Error") -- last line
+        highlight_line(-1, "Error") -- last line
 
         vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
       end
+
       -- Move focus back to the previous buffer.
       vim.api.nvim_input("<C-w>h")
     end
@@ -72,8 +64,10 @@ M.setup = function()
       -- TODO: Can you make this buffer read-only?
       vim.api.nvim_command("vnew")
       vim.api.nvim_command("setlocal buftype=nofile")
+
       -- Get the buffer number of the new buffer.
       local bufnr = vim.api.nvim_get_current_buf()
+
       -- Move focus back to the previous buffer.
       vim.api.nvim_input("<C-w>h")
 
@@ -92,4 +86,7 @@ M.setup = function()
   )
 end
 
-return M
+-- return M
+
+-- For local testing ...
+M.setup()
